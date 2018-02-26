45c1d87794331175740d92a0749db1995962e4e9
Mustapha Taiwo <mustyxerxes@hotmail.co.uk>
2018-02-26 14:06:15 +0100

    directly go to the dashboard for admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET admin listing. */
-router.get('/', function(req, res, next) {
-  res.send('this is the admin');
-});
-
-/**go deeper into admin */
-router.get('/dashboard', function(req, res, next){
+/**go to admin dashboard*/
+router.get('/', function(req, res, next){
     res.render('admin/index',{username:'John Philips'});
 });
 
