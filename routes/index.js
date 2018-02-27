65b74655a60c14113b5e72f3e25854b79f871361
Mustapha Taiwo <mustyxerxes@hotmail.co.uk>
2018-02-27 18:11:16 +0100

    just Program

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.Progam.findAll({}).then(function(programs){
+    res.render('index', { 
+      title: 'Express',
+      otherTitle: 'Sequelize: Express Example',
+      programs: programs
+    });
+  })
 });
 
 module.exports = router;
