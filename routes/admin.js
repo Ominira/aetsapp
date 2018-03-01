d64c10308fbc545d76128c4dedb78e44bf0dd260
Mustapha Taiwo <mustyxerxes@hotmail.co.uk>
2018-03-01 11:49:06 +0100

    yeap, now got it how it works

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /**go to admin dashboard*/
 router.get('/', function(req, res, next){
-    res.render('admin/index',{username:'John Philips'});
+    var name = 'John Philips';
+    if (req.query && req.query.username){
+        name = req.query.username;
+    }
+    models.Program.findAll({}).then(function(programs){
+        res.render('admin/index',{
+            username: name,
+            programs: programs
+        });
+    })
+});
+
+router.post('/', function(req, res, next){
+    //res.send(req.body);
+    var username = req.body.username;
+    res.redirect('/admin?username='+username);
 });
 
 module.exports = router;
