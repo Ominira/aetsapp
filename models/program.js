f17170e8ba6bfcd255b2c5b56d7b2e761ad3b69d
Mustapha Taiwo <mustyxerxes@hotmail.co.uk>
2018-03-01 11:47:24 +0100

    finally made it work

--- models/program.js
+++ models/program.js
@@ -1,10 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Program = sequelize.define('program', {
-    
+  var Program = sequelize.define('Program', {
+    program_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    program_desc_short: {
+      type: DataTypes.STRING
+    },
+    program_desc_long: {
+      type: DataTypes.STRING
+    }
   },{
-      tableName: 'program'
+      tableName: 'program',
+      timestamps: false
   });
-  
+
   return Program;
 };
