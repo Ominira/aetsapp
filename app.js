54ab2ee36722d7cdb33a419973e489cc44aa8c0d
Mustapha Taiwo <mustyxerxes@hotmail.co.uk>
2018-03-13 18:05:16 +0100

    add secret to cookieParser, playing around with res.locals and flash()

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(sessConfig.secret));
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (app.get('env') === 'production') {
