ad5dc00b42cd3a43bd5957fa4cafb79fb7dff789
Mustapha Taiwo <mustyxerxes@hotmail.co.uk>
2018-03-17 13:10:25 +0100

    wrap all within a function, fixes jshint issues

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,21 @@
-'use strict';
-
-var app = angular.module('aetsApp',[
-    'ngRoute',
-    'ngTouch',
-    'ngAnimate',
-    'ngAria',
-    'ngMessages',
-    'ui.bootstrap',
-    'toaster',
-    'ngMaterial'
-]);
-
-app.run(function($log, $rootScope, alertService){
-    if (!_.isEmpty(alertService.alerts)){
-        alertService.clear();
-    }
-    $rootScope.navOpen = false;
-});
+(function() { 
+    'use strict';
+    
+    var app = angular.module('aetsApp',[
+        'ngRoute',
+        'ngTouch',
+        'ngAnimate',
+        'ngAria',
+        'ngMessages',
+        'ui.bootstrap',
+        'toaster',
+        'ngMaterial'
+    ]);
+    
+    app.run(function($log, $rootScope, alertService){
+        if (!_.isEmpty(alertService.alerts)){
+            alertService.clear();
+        }
+        $rootScope.navOpen = false;
+    });
+})();
