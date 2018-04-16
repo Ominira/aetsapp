74bd10546876500927564912401ed7176aa8a651
Mustapha Taiwo <mustyxerxes@hotmail.co.uk>
2018-04-16 17:50:52 +0100

    use function version of 'use strict'

--- models/college.js
+++ models/college.js
@@ -1,33 +1,41 @@
-'use strict';
+(function() {
+    /**
+     * @author: Mustapha Taiwo
+     * @name: College Model
+     * @description: College Database Model
+     */
 
-module.exports = (sequelize, DataTypes) => {
-    const College = sequelize.define('College',{
-        collegeId: {
-            type: DataTypes.CHAR(10),
-            primaryKey: true,
-            field: 'college_id'
-        },
-        collegeDescriptionShort: {
-            type: DataTypes.STRING,
-            field: 'college_desc_short'
-        },
-        collegeDescriptionLong: {
-            type: DataTypes.STRING(360),
-            field: 'college_desc_long'
-        }
-    },{
-        tableName: 'college',
-        timestamps: false
-    });
+    'use strict';
 
-    College.associate = function(models) {
-        models.College.hasMany(models.Department,{
-            foreignKey: 'deptCollege',
-            sourceKey: 'collegeId'
+    module.exports = (sequelize, DataTypes) => {
+        const College = sequelize.define('College', {
+            collegeId: {
+                type: DataTypes.CHAR(10),
+                primaryKey: true,
+                field: 'college_id'
+            },
+            collegeDescriptionShort: {
+                type: DataTypes.STRING,
+                field: 'college_desc_short'
+            },
+            collegeDescriptionLong: {
+                type: DataTypes.STRING(360),
+                field: 'college_desc_long'
+            }
+        }, {
+            tableName: 'college',
+            timestamps: false
         });
+
+        College.associate = function(models) {
+            models.College.hasMany(models.Department, {
+                foreignKey: 'deptCollege',
+                sourceKey: 'collegeId'
+            });
+        };
+
+        College.sync();
+
+        return College;
     };
-
-    College.sync();
-
-    return College;
-}
+})();
